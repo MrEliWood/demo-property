96576b877acd9c9cff9f2675de0323e2a601c646
Eli Wood <mreliwood1@gmail.com>
2022-11-13 21:14:52 -0800

    set background video animations

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Vimeo from '@u-wave/react-vimeo';
 import './style.css';
 
@@ -9,8 +9,19 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ currentTab, setCurrentTab, data }) => {
+	// set default video state
+	const [videoState, setVideoState] = useState({ animation: 'none', height: '100vh' });
+
+	// get background video element from page
+	const video = document.getElementById('background-video');
+
 	// set current tab and header style based on user selection
 	const handleNavClick = (e: any) => {
+		if (currentTab === 'home') {
+			setVideoState({ animation: 'fade-out 1s both', height: '20vh' });
+		} else {
+			setVideoState({ animation: 'fade-in 1s both 1s', height: '100vh' });
+		}
 		switch (e.target.id) {
 			case 'nav-home':
 				setCurrentTab('home');
@@ -32,9 +43,15 @@ const Header: React.FC<Props> = ({ currentTab, setCurrentTab, data }) => {
 		}
 	};
 
+	// listen for background video load
+	useEffect(() => {
+		setVideoState({ animation: 'fade-in 1s both 1s', height: '100vh' });
+		// eslint-disable-next-line
+	}, [video]);
+
 	return (
 		<header className={currentTab === 'home' ? 'header' : 'header-collapsed'}>
-			{currentTab === 'home' && <Vimeo className='background-video' video='https://vimeo.com/692009875' height={window.innerHeight} controls={false} loop={true} muted={true} background={true} autoplay />}
+			<Vimeo id='background-video' className='background-video' style={videoState} video='https://vimeo.com/692009875' height={window.innerHeight} controls={false} loop={true} muted={true} background={true} autoplay />
 			<div className='header-banner'>
 				<a href={data.url} target='_blank' rel='noreferrer'>
 					<img className='brokerage-logo' src='./assets/logos/logo-light.png' alt='Listing agent logo' />
